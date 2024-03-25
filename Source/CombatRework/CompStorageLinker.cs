efe37e21a476b5042e53ef1b2858ad6f8ef6475b
Bennett Upfield <bjupfield@gmail.com>
2024-03-25 00:22:41 -0500

    working on adding the framework for dynamic recipes so we can have the repair function

---
+++ Source/CombatRework/CompStorageLinker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+
+namespace CombatRework
+{
+    public class CompStorageLinker : ThingComp 
+    {
+
+        private int maxconnectedCount = 10;
+
+        public List<Thing> connectedStorage = new List<Thing>();
+
+        public int MaxCount => maxconnectedCount;
+        public void ConnectStorage()
+        {
+                RimWorld.
+        }
+    }
+}
